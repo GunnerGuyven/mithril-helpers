62ca24322230ce51a161d237e3229485ed5658c4
Guyven <guyven@pm.me>
2025-06-11 13:16:09 -0400

    converted to tabs

--- form.ts
+++ form.ts
@@ -2,99 +2,99 @@ import m from "mithril"
 
 type SelectOption = string | [value: string, label: string]
 export type SelectAttrs = Partial<{
-  /**
-   * list of options to display in this select
-   * each item may be formatted as single string value,
-   * or tuple of strings [value, label]
-   */
-  options: SelectOption[]
-  /** enable multi-select (default: false) */
-  multiple: boolean
-  /** number of rows visible at a time, 0 is disabled (default: 0) */
-  size: number
-  /**
-   * A set of label and size pairs,
-   * options are added accordingly to groups in the order they are given
-   * [ [label, size], ... ]
-   */
-  groups: [label: string, size: number][]
-  /** the value(s) to pre-select */
-  selected: string[]
-  /** event fired on selection change */
-  onSelect: (values: string[]) => void
-  onSelectIndex: (index: number) => void
+	/**
+	 * list of options to display in this select
+	 * each item may be formatted as single string value,
+	 * or tuple of strings [value, label]
+	 */
+	options: SelectOption[]
+	/** enable multi-select (default: false) */
+	multiple: boolean
+	/** number of rows visible at a time, 0 is disabled (default: 0) */
+	size: number
+	/**
+	 * A set of label and size pairs,
+	 * options are added accordingly to groups in the order they are given
+	 * [ [label, size], ... ]
+	 */
+	groups: [label: string, size: number][]
+	/** the value(s) to pre-select */
+	selected: string[]
+	/** event fired on selection change */
+	onSelect: (values: string[]) => void
+	onSelectIndex: (index: number) => void
 }>
 export const Select: m.Component<SelectAttrs> = {
-  view: vnode => {
-    const {
-      options = [],
-      selected,
-      groups = [],
-      multiple,
-      size,
-      onSelect = () => {},
-      onSelectIndex = () => {},
-    } = vnode.attrs
+	view: vnode => {
+		const {
+			options = [],
+			selected,
+			groups = [],
+			multiple,
+			size,
+			onSelect = () => {},
+			onSelectIndex = () => {},
+		} = vnode.attrs
 
-    const makeOption = (opt: SelectOption) => {
-      const [value, label] = Array.isArray(opt) ? opt : [opt, opt]
-      return m("option", { value, selected: selected?.includes(value) }, label)
-    }
+		const makeOption = (opt: SelectOption) => {
+			const [value, label] = Array.isArray(opt) ? opt : [opt, opt]
+			return m("option", { value, selected: selected?.includes(value) }, label)
+		}
 
-    let optionsOffset = 0
-    const items = groups.map(([label, count]) =>
-      m(
-        "optgroup",
-        { label },
-        options.slice(optionsOffset, (optionsOffset += count)).map(makeOption),
-      ),
-    )
-    items.push(...options.slice(optionsOffset).map(makeOption))
+		let optionsOffset = 0
+		const items = groups.map(([label, count]) =>
+			m(
+				"optgroup",
+				{ label },
+				options.slice(optionsOffset, (optionsOffset += count)).map(makeOption),
+			),
+		)
+		items.push(...options.slice(optionsOffset).map(makeOption))
 
-    return m(
-      "select",
-      {
-        oninput: (e: { target: HTMLSelectElement }) => (
-          onSelect([...e.target.selectedOptions].map(o => o.value)),
-          onSelectIndex(e.target.selectedIndex)
-        ),
-        multiple,
-        size,
-      },
-      items,
-    )
-  },
+		return m(
+			"select",
+			{
+				oninput: (e: { target: HTMLSelectElement }) => (
+					onSelect([...e.target.selectedOptions].map(o => o.value)),
+					onSelectIndex(e.target.selectedIndex)
+				),
+				multiple,
+				size,
+			},
+			items,
+		)
+	},
 }
 
 export const SelectStateful: m.ClosureComponent<{
-  state?: SelectAttrs
-  whenOptionChangeResetSelected?: boolean
+	state?: SelectAttrs
+	whenOptionChangeResetSelected?: boolean
 }> = initialVnode => {
-  const { state = {}, whenOptionChangeResetSelected = false } =
-    initialVnode.attrs
-  const _select = state.onSelect
-  state.onSelect = value => {
-    _select && _select(value), (state.selected = value)
-  }
-  const resetSelected = () => {
-    if (state.options?.length) {
-      const o = state.options[0]
-      state.selected = [Array.isArray(o) ? o[0] : o]
-      _select && _select(state.selected)
-    }
-  }
-  state.selected || resetSelected()
-  let lastOptions = state.options
-  return {
-    view: vnode => {
-      if (whenOptionChangeResetSelected) {
-        const newOptions = vnode.attrs.state?.options
-        if (newOptions != lastOptions) {
-          lastOptions = newOptions
-          resetSelected()
-        }
-      }
-      return m(Select, state)
-    },
-  }
+	const { state = {}, whenOptionChangeResetSelected = false } =
+		initialVnode.attrs
+	const _select = state.onSelect
+	state.onSelect = value => {
+		_select && _select(value), (state.selected = value)
+	}
+	const resetSelected = () => {
+		if (state.options?.length) {
+			const o = state.options[0]
+			state.selected = [Array.isArray(o) ? o[0] : o]
+			_select && _select(state.selected)
+		}
+	}
+	state.selected || resetSelected()
+	let lastOptions = state.options
+	return {
+		view: vnode => {
+			if (whenOptionChangeResetSelected) {
+				const newOptions = vnode.attrs.state?.options
+				if (newOptions != lastOptions) {
+					lastOptions = newOptions
+					resetSelected()
+				}
+			}
+			return m(Select, state)
+		},
+	}
 }
--- table.ts
+++ table.ts
@@ -2,319 +2,319 @@ import m, { Children } from "mithril"
 import { Select } from "./form.ts"
 
 type PaginationStyle = {
-  parent: string
-  prev: string
-  next: string
-  itemContainer: string
-  itemOuter?: string
-  item: string
-  itemSkip: string
-  itemCurrent: string
-  pageSizeContainer?: string
+	parent: string
+	prev: string
+	next: string
+	itemContainer: string
+	itemOuter?: string
+	item: string
+	itemSkip: string
+	itemCurrent: string
+	pageSizeContainer?: string
 }
 
 type PaginationAttrs = Partial<{
-  total: number
-  selected: number
-  onPageChange: (newPageNum: number) => void
+	total: number
+	selected: number
+	onPageChange: (newPageNum: number) => void
 
-  pageSize: number
-  pageSizeOptions: number[]
-  onPageSizeChange: (newPageSizeNum: number) => void
+	pageSize: number
+	pageSizeOptions: number[]
+	onPageSizeChange: (newPageSizeNum: number) => void
 
-  proximity: number
-  nextIndicator: m.Child
-  prevIndicator: m.Child
-  skipIndicator: m.Child
-  style: PaginationStyle
+	proximity: number
+	nextIndicator: m.Child
+	prevIndicator: m.Child
+	skipIndicator: m.Child
+	style: PaginationStyle
 }>
 
 export const bulmaPaginationStyle: PaginationStyle = {
-  parent: "nav.pagination",
+	parent: "nav.pagination",
 
-  prev: "a.pagination-previous",
-  next: "a.pagination-next",
+	prev: "a.pagination-previous",
+	next: "a.pagination-next",
 
-  itemContainer: "ul.pagination-list",
-  itemOuter: "li",
-  item: "a.pagination-link",
-  itemSkip: "span.pagination-ellipsis",
-  itemCurrent: "a.pagination-link.is-current",
+	itemContainer: "ul.pagination-list",
+	itemOuter: "li",
+	item: "a.pagination-link",
+	itemSkip: "span.pagination-ellipsis",
+	itemCurrent: "a.pagination-link.is-current",
 
-  pageSizeContainer: ".select",
+	pageSizeContainer: ".select",
 }
 
 export const PaginationStateful: m.ClosureComponent<{
-  state?: PaginationAttrs
+	state?: PaginationAttrs
 }> = initialVnode => {
-  const { state = {} } = initialVnode.attrs
-  const _change = state.onPageChange
-  state.onPageChange = pageNumber => {
-    _change && _change(pageNumber), (state.selected = pageNumber)
-  }
+	const { state = {} } = initialVnode.attrs
+	const _change = state.onPageChange
+	state.onPageChange = pageNumber => {
+		_change && _change(pageNumber), (state.selected = pageNumber)
+	}
 
-  return { view: () => m(Pagination, state) }
+	return { view: () => m(Pagination, state) }
 }
 
 export const Pagination: m.Component<PaginationAttrs> = {
-  view: vNode => {
-    const {
-      total = 0,
-      selected = 1,
-      onPageChange = () => {},
+	view: vNode => {
+		const {
+			total = 0,
+			selected = 1,
+			onPageChange = () => {},
 
-      pageSize,
-      pageSizeOptions = [5, 10, 15, 20],
-      onPageSizeChange = () => {},
+			pageSize,
+			pageSizeOptions = [5, 10, 15, 20],
+			onPageSizeChange = () => {},
 
-      proximity = 2,
-      prevIndicator = "←", // "←" "⇽" "⟵" "⇐" "⊲" "‹" "◃" "◂" "≺" "≪" "⋘" "⦑" "⟨" "⟪" "⧏" "⪡" "⪻" "«" "⪦"
-      nextIndicator = "→", // "→" "⇾" "⟶" "⇒" "⊳" "›" "▹" "▸" "≻" "≫" "⋙" "⦒" "⟩" "⟫" "⧐" "⪢" "⪼" "»" "⪧"
-      skipIndicator = "…", // "‥"
-      style = bulmaPaginationStyle,
-    } = vNode.attrs
+			proximity = 2,
+			prevIndicator = "←", // "←" "⇽" "⟵" "⇐" "⊲" "‹" "◃" "◂" "≺" "≪" "⋘" "⦑" "⟨" "⟪" "⧏" "⪡" "⪻" "«" "⪦"
+			nextIndicator = "→", // "→" "⇾" "⟶" "⇒" "⊳" "›" "▹" "▸" "≻" "≫" "⋙" "⦒" "⟩" "⟫" "⧐" "⪢" "⪼" "»" "⪧"
+			skipIndicator = "…", // "‥"
+			style = bulmaPaginationStyle,
+		} = vNode.attrs
 
-    const item =
-      style.itemOuter ?
-        (num: number, current = false) =>
-          m(
-            style.itemOuter || "",
-            { key: `p${num}` },
-            m(
-              current ? style.itemCurrent : style.item,
-              {
-                "aria-label": `Go to Page ${num}`,
-                onclick: () => onPageChange(num),
-              },
-              num,
-            ),
-          )
-      : (num: number, current = false) =>
-          m(
-            current ? style.itemCurrent : style.item,
-            {
-              key: `p${num}`,
-              "aria-label": `Go to Page ${num}`,
-              onclick: () => onPageChange(num),
-            },
-            num,
-          )
+		const item =
+			style.itemOuter ?
+				(num: number, current = false) =>
+					m(
+						style.itemOuter || "",
+						{ key: `p${num}` },
+						m(
+							current ? style.itemCurrent : style.item,
+							{
+								"aria-label": `Go to Page ${num}`,
+								onclick: () => onPageChange(num),
+							},
+							num,
+						),
+					)
+			:	(num: number, current = false) =>
+					m(
+						current ? style.itemCurrent : style.item,
+						{
+							key: `p${num}`,
+							"aria-label": `Go to Page ${num}`,
+							onclick: () => onPageChange(num),
+						},
+						num,
+					)
 
-    const skipR =
-      style.itemOuter ?
-        m(style.itemOuter, { key: "skipR" }, m(style.itemSkip, skipIndicator))
-      : m(style.itemSkip, { key: "skipR" }, skipIndicator)
-    const skipL =
-      style.itemOuter ?
-        m(style.itemOuter, { key: "skipL" }, m(style.itemSkip, skipIndicator))
-      : m(style.itemSkip, { key: "skipL" }, skipIndicator)
+		const skipR =
+			style.itemOuter ?
+				m(style.itemOuter, { key: "skipR" }, m(style.itemSkip, skipIndicator))
+			:	m(style.itemSkip, { key: "skipR" }, skipIndicator)
+		const skipL =
+			style.itemOuter ?
+				m(style.itemOuter, { key: "skipL" }, m(style.itemSkip, skipIndicator))
+			:	m(style.itemSkip, { key: "skipL" }, skipIndicator)
 
-    function* range(start: number, end: number, map: (v: number) => m.Child) {
-      for (let i = start; i <= end; i++) {
-        yield map(i)
-      }
-    }
+		function* range(start: number, end: number, map: (v: number) => m.Child) {
+			for (let i = start; i <= end; i++) {
+				yield map(i)
+			}
+		}
 
-    const pageSelect = m(Select, {
-      options: pageSizeOptions.map(n => n.toFixed()),
-      selected: pageSize ? [pageSize.toFixed()] : undefined,
-      onSelect: s => onPageSizeChange(Number(s[0])),
-    })
+		const pageSelect = m(Select, {
+			options: pageSizeOptions.map(n => n.toFixed()),
+			selected: pageSize ? [pageSize.toFixed()] : undefined,
+			onSelect: s => onPageSizeChange(Number(s[0])),
+		})
 
-    const list = (selected: number) => [
-      ...(selected > proximity + 3 ?
-        [item(1), skipL, ...range(selected - proximity, selected - 1, item)]
-      : range(1, selected - 1, item)),
-      item(selected, true),
-      ...(selected < total - proximity - 2 ?
-        [...range(selected + 1, selected + proximity, item), skipR, item(total)]
-      : range(selected + 1, total, item)),
-    ]
-    return m(
-      style.parent,
-      { role: "navigation", "aria-label": "pagination" },
-      pageSize &&
-        (style.pageSizeContainer ?
-          m(style.pageSizeContainer, pageSelect)
-        : pageSelect),
-      m(
-        style.prev,
-        {
-          onclick: () => selected > 1 && onPageChange(selected - 1),
-          disabled: selected <= 1,
-        },
-        prevIndicator,
-      ),
-      m(
-        style.next,
-        {
-          onclick: () => selected < total && onPageChange(selected + 1),
-          disabled: selected >= total,
-        },
-        nextIndicator,
-      ),
-      m(style.itemContainer, list(selected)),
-    )
-  },
+		const list = (selected: number) => [
+			...(selected > proximity + 3 ?
+				[item(1), skipL, ...range(selected - proximity, selected - 1, item)]
+			:	range(1, selected - 1, item)),
+			item(selected, true),
+			...(selected < total - proximity - 2 ?
+				[...range(selected + 1, selected + proximity, item), skipR, item(total)]
+			:	range(selected + 1, total, item)),
+		]
+		return m(
+			style.parent,
+			{ role: "navigation", "aria-label": "pagination" },
+			pageSize &&
+				(style.pageSizeContainer ?
+					m(style.pageSizeContainer, pageSelect)
+				:	pageSelect),
+			m(
+				style.prev,
+				{
+					onclick: () => selected > 1 && onPageChange(selected - 1),
+					disabled: selected <= 1,
+				},
+				prevIndicator,
+			),
+			m(
+				style.next,
+				{
+					onclick: () => selected < total && onPageChange(selected + 1),
+					disabled: selected >= total,
+				},
+				nextIndicator,
+			),
+			m(style.itemContainer, list(selected)),
+		)
+	},
 }
 
 type GridDataRow = { cells: Children[]; subGrid?: GridData }
 export type GridData = {
-  meta: Partial<{ subGridFieldIdx: number; showHeader: boolean }>
-  columns: Children[]
-  rows: GridDataRow[]
+	meta: Partial<{ subGridFieldIdx: number; showHeader: boolean }>
+	columns: Children[]
+	rows: GridDataRow[]
 }
 
 const GridField: m.Component<{ isHeader?: boolean; value: Children }> = {
-  view: vnode => {
-    const { value, isHeader = false } = vnode.attrs
-    return m(
-      isHeader ? "th" : "td",
-      value === true ? "true"
-      : value === false ? "false"
-      : value,
-    )
-  },
+	view: vnode => {
+		const { value, isHeader = false } = vnode.attrs
+		return m(
+			isHeader ? "th" : "td",
+			value === true ? "true"
+			: value === false ? "false"
+			: value,
+		)
+	},
 }
 
 const GridHeaderRow: m.Component<{ columns: Children[] }> = {
-  view: vnode => {
-    const { columns } = vnode.attrs
-    return m(
-      "tr",
-      columns.map(f => m(GridField, { value: f, isHeader: true })),
-    )
-  },
+	view: vnode => {
+		const { columns } = vnode.attrs
+		return m(
+			"tr",
+			columns.map(f => m(GridField, { value: f, isHeader: true })),
+		)
+	},
 }
 
 const GridRow: m.Component<{
-  row: GridDataRow
-  renderKey: string | number
-  subGridFieldIdx: number
+	row: GridDataRow
+	renderKey: string | number
+	subGridFieldIdx: number
 }> = {
-  view: vnode => {
-    const { row, renderKey, subGridFieldIdx } = vnode.attrs
-    const cells: Children[] = row.cells.map(f => m(GridField, { value: f }))
-    row.subGrid &&
-      cells.splice(subGridFieldIdx, 0, m(Grid, { data: row.subGrid }))
-    return m(`tr[key-in=${renderKey}]`, { key: renderKey }, cells)
-  },
+	view: vnode => {
+		const { row, renderKey, subGridFieldIdx } = vnode.attrs
+		const cells: Children[] = row.cells.map(f => m(GridField, { value: f }))
+		row.subGrid &&
+			cells.splice(subGridFieldIdx, 0, m(Grid, { data: row.subGrid }))
+		return m(`tr[key-in=${renderKey}]`, { key: renderKey }, cells)
+	},
 }
 
 export const Grid: m.Component<{ data?: GridData }> = {
-  view: vnode => {
-    const { data } = vnode.attrs
-    const { meta, rows, columns } = data || {
-      meta: { showHeader: true },
-      rows: [],
-      columns: [],
-    }
-    return columns.length ?
-        m(
-          "table",
-          meta.showHeader && m("thead", m(GridHeaderRow, { columns })),
-          rows.map((row, idx) =>
-            m(GridRow, {
-              row,
-              renderKey: idx,
-              subGridFieldIdx: meta.subGridFieldIdx || -1,
-            }),
-          ),
-        )
-      : m("", "Empty Result")
-  },
+	view: vnode => {
+		const { data } = vnode.attrs
+		const { meta, rows, columns } = data || {
+			meta: { showHeader: true },
+			rows: [],
+			columns: [],
+		}
+		return columns.length ?
+				m(
+					"table",
+					meta.showHeader && m("thead", m(GridHeaderRow, { columns })),
+					rows.map((row, idx) =>
+						m(GridRow, {
+							row,
+							renderKey: idx,
+							subGridFieldIdx: meta.subGridFieldIdx || -1,
+						}),
+					),
+				)
+			:	m("", "Empty Result")
+	},
 }
 
 export type PagedGridData = GridData & { paginationProps: PaginationAttrs }
 export const PagedGrid: m.Component<
-  Partial<{
-    data: PagedGridData
-    paginationAbove: boolean
-    paginationBelow: boolean
-  }>
+	Partial<{
+		data: PagedGridData
+		paginationAbove: boolean
+		paginationBelow: boolean
+	}>
 > = {
-  view: vnode => {
-    const {
-      data,
-      paginationAbove = false,
-      paginationBelow = true,
-    } = vnode.attrs
+	view: vnode => {
+		const {
+			data,
+			paginationAbove = false,
+			paginationBelow = true,
+		} = vnode.attrs
 
-    return [
-      paginationAbove && m(Pagination, data?.paginationProps || {}),
-      m(Grid, { data }),
-      paginationBelow && m(Pagination, data?.paginationProps || {}),
-    ]
-  },
+		return [
+			paginationAbove && m(Pagination, data?.paginationProps || {}),
+			m(Grid, { data }),
+			paginationBelow && m(Pagination, data?.paginationProps || {}),
+		]
+	},
 }
 
 type GridDataSubItem = Record<string, Children>[]
 export const createGridData = (
-  data?: Record<string, Children | GridDataSubItem>[],
-  {
-    showHeader = true,
-    autoDetectSubGridField = true,
-    subGridField,
-    subGridFieldIdx = -1,
-  }: Partial<{
-    showHeader: boolean
-    subGridField: string
-    subGridFieldIdx: number
-    autoDetectSubGridField: boolean
-  }> = {},
+	data?: Record<string, Children | GridDataSubItem>[],
+	{
+		showHeader = true,
+		autoDetectSubGridField = true,
+		subGridField,
+		subGridFieldIdx = -1,
+	}: Partial<{
+		showHeader: boolean
+		subGridField: string
+		subGridFieldIdx: number
+		autoDetectSubGridField: boolean
+	}> = {},
 ): GridData | undefined => {
-  if (!data || !data.length) return undefined
-  const columns = Object.keys(data[0])
-  subGridFieldIdx = subGridField ? columns.indexOf(subGridField) : -1
-  if (subGridFieldIdx === -1 && autoDetectSubGridField) {
-    subGridFieldIdx = Object.values(data[0]).findIndex(v => Array.isArray(v))
-  }
+	if (!data || !data.length) return undefined
+	const columns = Object.keys(data[0])
+	subGridFieldIdx = subGridField ? columns.indexOf(subGridField) : -1
+	if (subGridFieldIdx === -1 && autoDetectSubGridField) {
+		subGridFieldIdx = Object.values(data[0]).findIndex(v => Array.isArray(v))
+	}
 
-  const rows = data.map(r => {
-    const x = Object.values(r)
-    const subGrid =
-      subGridFieldIdx > -1 &&
-      (x.splice(subGridFieldIdx, 1)[0] as GridDataSubItem)
-    return {
-      cells: x as Children[],
-      subGrid: createGridData(subGrid || undefined),
-    }
-  })
+	const rows = data.map(r => {
+		const x = Object.values(r)
+		const subGrid =
+			subGridFieldIdx > -1 &&
+			(x.splice(subGridFieldIdx, 1)[0] as GridDataSubItem)
+		return {
+			cells: x as Children[],
+			subGrid: createGridData(subGrid || undefined),
+		}
+	})
 
-  return { rows, columns, meta: { subGridFieldIdx, showHeader } }
+	return { rows, columns, meta: { subGridFieldIdx, showHeader } }
 }
 
 export const paginateGridData = (
-  gridData?: GridData,
-  paging?: Partial<{ pageSize: number; currentPage: number }>,
+	gridData?: GridData,
+	paging?: Partial<{ pageSize: number; currentPage: number }>,
 ): PagedGridData | undefined => {
-  if (!gridData) return undefined
-  const { pageSize = 10, currentPage = 1 } = paging || {}
-  const p = {
-    selected: currentPage,
-    pageSize,
-    get total() {
-      return Math.ceil(gridData.rows.length / p.pageSize)
-    },
-    onPageSizeChange: (size: number) => (
-      (p.pageSize = size),
-      (p.selected = Math.min(p.selected, p.total)),
-      (_page = pageTo(p.selected))
-    ),
-    onPageChange: (newPage: number) => (
-      (p.selected = newPage), (_page = pageTo(p.selected))
-    ),
-  }
-  const pageTo = (num: number) =>
-    gridData.rows.slice((num - 1) * p.pageSize, num * p.pageSize)
-  let _page = pageTo(currentPage)
+	if (!gridData) return undefined
+	const { pageSize = 10, currentPage = 1 } = paging || {}
+	const p = {
+		selected: currentPage,
+		pageSize,
+		get total() {
+			return Math.ceil(gridData.rows.length / p.pageSize)
+		},
+		onPageSizeChange: (size: number) => (
+			(p.pageSize = size),
+			(p.selected = Math.min(p.selected, p.total)),
+			(_page = pageTo(p.selected))
+		),
+		onPageChange: (newPage: number) => (
+			(p.selected = newPage), (_page = pageTo(p.selected))
+		),
+	}
+	const pageTo = (num: number) =>
+		gridData.rows.slice((num - 1) * p.pageSize, num * p.pageSize)
+	let _page = pageTo(currentPage)
 
-  // mixing spread with get does not work before ES2018
-  return {
-    ...gridData,
-    get rows() {
-      return _page
-    },
-    paginationProps: p,
-  }
+	// mixing spread with get does not work before ES2018
+	return {
+		...gridData,
+		get rows() {
+			return _page
+		},
+		paginationProps: p,
+	}
 }
